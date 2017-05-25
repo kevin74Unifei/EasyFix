80af82198754e4f027d0a9029e7ffd0b4938b2a7
Rodrigo Maia <Rodrigo Maia@DESKTOP-SI5CMF9>
2017-05-24 23:08:50 -0300

    Completo CRUD funcionario 2

--- resources/views/crud-funcionario/funcionariosList.blade.php
+++ resources/views/crud-funcionario/funcionariosList.blade.php
@@ -61,8 +61,7 @@
 </div>
     
 
-<div class="pagina">
-    
+<div class="pagina">    
     
     <!--LISTA DE FUNCIONARIOS-->
     <table class="table">
@@ -79,7 +78,7 @@
         <tr>
             <th colspan="2">
                 <a href="{{url("funcionario/show/".$f['func_cod'])}}" class="list-group-item" style="height:100px;width:620px;">  
-                    <img src="http://localhost/sissarproject/storage/app/imgperfil/{{$f['func_imagem'] or 'avatar.png'}}" style="width:51px;height:72px;" alt="perfil_foto">
+                    <img src="{{url('storage/imgperfil/')."/"}}{{$f['func_imagem'] or 'avatar.png'}}" style="width:51px;height:72px;" alt="perfil_foto">
                     <div style="position:relative;top:-92px;left:70px;width:500px;">                        
                         <h3>{{$f['func_nome']}}</h3>                         
                         <label>CPF: {{$f['func_CPF']}}</label><br/>
